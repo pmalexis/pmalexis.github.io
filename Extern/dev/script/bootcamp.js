163475bb790f1547e37765a47a7da149cd42ded9
Alexis Prevost-Maynen <alexis@callbruno.com>
2022-02-10 17:03:35 +0100

    Update EXTERN

--- Extern/dev/script/bootcamp.js
+++ Extern/dev/script/bootcamp.js
@@ -1,198 +1,197 @@
-let section3Quote = document.querySelectorAll('.bootcamp-section3');
-section3Quote.forEach(function (section) {
-    let click = true;
-    let position = 1;
-    let quotes = section.querySelectorAll('.bootcamp-section3_quote')
-    section.querySelector('.bootcamp-section3_btnprev').addEventListener('click', function () {
-        if(click) {
-            position -= 1;
-            if(position === 0) position = quotes.length;
-            update(position);
-        }
-    });
-    section.querySelector('.bootcamp-section3_btnnext').addEventListener('click', function () {
-        if(click) {
-            position += 1;
-            if(position > quotes.length) position = 1;
-            update(position);
-        }
-    });
-
-    function update(pos) {
-        let position = pos;
-        click = false;
-        setTimeout(function(){
-            click = true;
-        }, 500);
-
-        quotes.forEach(function (quote, index) {
-            if(index+1 === position) {
-                setTimeout(function(){
-                    quote.classList.remove('style-none');
-                    setTimeout(function(){
-                        quote.classList.remove('style-hide');
-                    }, 10);
-                }, 500);
-            } else {
-                quote.classList.add('style-hide');
-                setTimeout(function(){
-                    quote.classList.add('style-none');
-                }, 500);
-            }
-        });
-    }
-
-    function init() {
-        quotes.forEach(function (quote, index) {
-            if(index != 0) {
-                quote.classList.add('style-hide');
-                quote.classList.add('style-none');
-            }
-        });
-    }
-    init();
-});
-
-
-let section7BtnBis = document.querySelectorAll('.home-section7-containerbtn');
-let section7CountBis = 1;
-let section7NumQuoteBis = document.querySelectorAll('.home-section7-quote').length;
-section7BtnBis.forEach(function (btn, index) {
-    document.querySelector('.home-section7-quote:nth-child(' + section7CountBis + ')').classList.add('style-show'); 
-    btn.addEventListener('click', function () {
-        if(index == 0){
-            if (section7CountBis > 1){
-                section7CountBis --;
-            }
-            
-        } else if(index == 1) {
-            
-            if (section7CountBis < section7NumQuoteBis){
-                section7CountBis ++;
-            }
-        }
-        
-        section7QuotesBis = document.querySelectorAll('.home-section7-quote');
-        section7QuotesBis.forEach(function (quote) {
-            quote.classList.remove('style-show');
-        });
-
-        document.querySelector('.home-section7-quote:nth-child(' + section7CountBis + ')').classList.add('style-show'); 
-    });
-});
-
-
-let section4Tab = document.querySelectorAll('.bootcamp-section4');
-section4Tab.forEach(function (section) {
-    let click = true;
-    let position = 0;
-    
-    let tabs = section.querySelectorAll('.bootcamp-section4_tab')
-    let lists = section.querySelectorAll('.bootcamp-section4_contenttab')
-
-    let numberOfWeek = parseInt(section.querySelector('.bootcamp-section4_containertab .bootcamp-section4_tab:first-child .bootcamp-section4_number').textContent)
-    let NumberOfWeekTotal = parseInt(section.querySelector('.bootcamp-section4_containerprogressbar .bootcamp-section4_containerstep .bootcamp-section4_step:last-child .h6').textContent);
-    let progressBar = section.querySelector('.bootcamp-section4_containerprogressbar .bootcamp-section4_progressbar .bootcamp-section4_bar');
-
-    tabs.forEach(function (tab) {
-        tab.addEventListener('click', function () {
+if( document.querySelector('.bootcamp-sectionhero') ) {
+    let section3Quote = document.querySelectorAll('.bootcamp-section3');
+    section3Quote.forEach(function (section) {
+        let click = true;
+        let position = 1;
+        let quotes = section.querySelectorAll('.bootcamp-section3_quote')
+        section.querySelector('.bootcamp-section3_btnprev').addEventListener('click', function () {
             if(click) {
-                click = false;
-                setTimeout(function(){
-                    click = true;
-                }, 250);
-                tabs[position].classList.remove('style-active');
-                lists[position].classList.remove('style-show');
-                position = Array.prototype.slice.call(this.parentElement.children).indexOf(this);
-                this.classList.add('style-active');
-                lists[position].classList.add('style-show');
+                position -= 1;
+                if(position === 0) position = quotes.length;
+                update(position);
+            }
+        });
+        section.querySelector('.bootcamp-section3_btnnext').addEventListener('click', function () {
+            if(click) {
+                position += 1;
+                if(position > quotes.length) position = 1;
+                update(position);
+            }
+        });
 
-                //update numberOfWeek
-                numberOfWeek = 0;
-                for (let i = 0; i <= position; i++) {
-                    numberOfWeek += parseInt(tabs[i].querySelector('.bootcamp-section4_number').textContent)
+        function update(pos) {
+            let position = pos;
+            click = false;
+            setTimeout(function(){
+                click = true;
+            }, 500);
+
+            quotes.forEach(function (quote, index) {
+                if(index+1 === position) {
+                    setTimeout(function(){
+                        quote.classList.remove('style-none');
+                        setTimeout(function(){
+                            quote.classList.remove('style-hide');
+                        }, 10);
+                    }, 500);
+                } else {
+                    quote.classList.add('style-hide');
+                    setTimeout(function(){
+                        quote.classList.add('style-none');
+                    }, 500);
+                }
+            });
+        }
+
+        function init() {
+            quotes.forEach(function (quote, index) {
+                if(index != 0) {
+                    quote.classList.add('style-hide');
+                    quote.classList.add('style-none');
+                }
+            });
+        }
+        init();
+    });
+
+    let section7BtnBis = document.querySelectorAll('.home-section7-containerbtn');
+    let section7CountBis = 1;
+    let section7NumQuoteBis = document.querySelectorAll('.home-section7-quote').length;
+    section7BtnBis.forEach(function (btn, index) {
+        document.querySelector('.home-section7-quote:nth-child(' + section7CountBis + ')').classList.add('style-show'); 
+        btn.addEventListener('click', function () {
+            if(index == 0){
+                if (section7CountBis > 1){
+                    section7CountBis --;
                 }
                 
-                let slideData = -100 + ( numberOfWeek * 100 / NumberOfWeekTotal );
-                progressBar.style.webkitTransfoprogressBarrm = 'translateX(' + slideData + '%' + ') translateZ(0)';
-                progressBar.style.MozTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
-                progressBar.style.msTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
-                progressBar.style.OTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
-                progressBar.style.transform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+            } else if(index == 1) {
+                
+                if (section7CountBis < section7NumQuoteBis){
+                    section7CountBis ++;
+                }
             }
+            
+            section7QuotesBis = document.querySelectorAll('.home-section7-quote');
+            section7QuotesBis.forEach(function (quote) {
+                quote.classList.remove('style-show');
+            });
+
+            document.querySelector('.home-section7-quote:nth-child(' + section7CountBis + ')').classList.add('style-show'); 
         });
     });
 
-    function update(pos) {
+    let section4Tab = document.querySelectorAll('.bootcamp-section4');
+    section4Tab.forEach(function (section) {
+        let click = true;
+        let position = 0;
         
-    }
+        let tabs = section.querySelectorAll('.bootcamp-section4_tab')
+        let lists = section.querySelectorAll('.bootcamp-section4_contenttab')
 
-    function init() {
-        tabs[0].classList.add('style-active');
-        lists[0].classList.add('style-show');
+        let numberOfWeek = parseInt(section.querySelector('.bootcamp-section4_containertab .bootcamp-section4_tab:first-child .bootcamp-section4_number').textContent)
+        let NumberOfWeekTotal = parseInt(section.querySelector('.bootcamp-section4_containerprogressbar .bootcamp-section4_containerstep .bootcamp-section4_step:last-child .h6').textContent);
+        let progressBar = section.querySelector('.bootcamp-section4_containerprogressbar .bootcamp-section4_progressbar .bootcamp-section4_bar');
 
-        let slideData = -100 + ( numberOfWeek * 100 / NumberOfWeekTotal );
-        progressBar.style.webkitTransfoprogressBarrm = 'translateX(' + slideData + '%' + ') translateZ(0)';
-        progressBar.style.MozTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
-        progressBar.style.msTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
-        progressBar.style.OTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
-        progressBar.style.transform = 'translateX(' + slideData + '%' + ') translateZ(0)';
-    }
-    init();
-});
-
-
-let section6Card = document.querySelectorAll('.bootcamp-section6');
-section6Card.forEach(function (section) {
-    let click = true;
-    let position = 1;
-    let quotes = section.querySelectorAll('.bootcamp-section6_listcard')
-    section.querySelector('.bootcamp-section6_btnprev').addEventListener('click', function () {
-        if(click) {
-            position -= 1;
-            if(position === 0) position = quotes.length;
-            update(position);
-        }
-    });
-    section.querySelector('.bootcamp-section6_btnnext').addEventListener('click', function () {
-        if(click) {
-            position += 1;
-            if(position > quotes.length) position = 1;
-            update(position);
-        }
-    });
-
-    function update(pos) {
-        let position = pos;
-        click = false;
-        setTimeout(function(){
-            click = true;
-        }, 500);
-
-        quotes.forEach(function (quote, index) {
-            if(index+1 === position) {
-                setTimeout(function(){
-                    quote.classList.remove('style-none');
+        tabs.forEach(function (tab) {
+            tab.addEventListener('click', function () {
+                if(click) {
+                    click = false;
                     setTimeout(function(){
-                        quote.classList.remove('style-hide');
-                    }, 10);
-                }, 500);
-            } else {
-                quote.classList.add('style-hide');
-                setTimeout(function(){
-                    quote.classList.add('style-none');
-                }, 500);
-            }
-        });
-    }
+                        click = true;
+                    }, 250);
+                    tabs[position].classList.remove('style-active');
+                    lists[position].classList.remove('style-show');
+                    position = Array.prototype.slice.call(this.parentElement.children).indexOf(this);
+                    this.classList.add('style-active');
+                    lists[position].classList.add('style-show');
 
-    function init() {
-        quotes.forEach(function (quote, index) {
-            if(index != 0) {
-                quote.classList.add('style-hide');
-                quote.classList.add('style-none');
+                    //update numberOfWeek
+                    numberOfWeek = 0;
+                    for (let i = 0; i <= position; i++) {
+                        numberOfWeek += parseInt(tabs[i].querySelector('.bootcamp-section4_number').textContent)
+                    }
+                    
+                    let slideData = -100 + ( numberOfWeek * 100 / NumberOfWeekTotal );
+                    progressBar.style.webkitTransfoprogressBarrm = 'translateX(' + slideData + '%' + ') translateZ(0)';
+                    progressBar.style.MozTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+                    progressBar.style.msTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+                    progressBar.style.OTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+                    progressBar.style.transform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+                }
+            });
+        });
+
+        function update(pos) {
+            
+        }
+
+        function init() {
+            tabs[0].classList.add('style-active');
+            lists[0].classList.add('style-show');
+
+            let slideData = -100 + ( numberOfWeek * 100 / NumberOfWeekTotal );
+            progressBar.style.webkitTransfoprogressBarrm = 'translateX(' + slideData + '%' + ') translateZ(0)';
+            progressBar.style.MozTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+            progressBar.style.msTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+            progressBar.style.OTransform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+            progressBar.style.transform = 'translateX(' + slideData + '%' + ') translateZ(0)';
+        }
+        init();
+    });
+
+    let section6Card = document.querySelectorAll('.bootcamp-section6');
+    section6Card.forEach(function (section) {
+        let click = true;
+        let position = 1;
+        let quotes = section.querySelectorAll('.bootcamp-section6_listcard')
+        section.querySelector('.bootcamp-section6_btnprev').addEventListener('click', function () {
+            if(click) {
+                position -= 1;
+                if(position === 0) position = quotes.length;
+                update(position);
             }
         });
-    }
-    init();
-});
+        section.querySelector('.bootcamp-section6_btnnext').addEventListener('click', function () {
+            if(click) {
+                position += 1;
+                if(position > quotes.length) position = 1;
+                update(position);
+            }
+        });
+
+        function update(pos) {
+            let position = pos;
+            click = false;
+            setTimeout(function(){
+                click = true;
+            }, 500);
+
+            quotes.forEach(function (quote, index) {
+                if(index+1 === position) {
+                    setTimeout(function(){
+                        quote.classList.remove('style-none');
+                        setTimeout(function(){
+                            quote.classList.remove('style-hide');
+                        }, 10);
+                    }, 500);
+                } else {
+                    quote.classList.add('style-hide');
+                    setTimeout(function(){
+                        quote.classList.add('style-none');
+                    }, 500);
+                }
+            });
+        }
+
+        function init() {
+            quotes.forEach(function (quote, index) {
+                if(index != 0) {
+                    quote.classList.add('style-hide');
+                    quote.classList.add('style-none');
+                }
+            });
+        }
+        init();
+    });
+}
